ad4b278cc3c4714890fa29f29c10ee457f661acf
jaervin1 <jaervin@email.sc.edu>
2025-02-23 19:54:10 -0500

    Assignment 7

--- exercises/js_loops/loops.js
+++ exercises/js_loops/loops.js
@@ -1,10 +1,69 @@
 document.getElementById("btn-display").onclick = () => {
-  const startNum = document.getElementById("input-start").value;
-  const endNum = document.getElementById("input-end").value;
-  if (startNum > endNum) {
-    document.getElementById(
-      "number-error"
-    ).innerHTML = `${endNum} is less than ${startNum}.`;
-    return;
+  const startNum = parseInt(document.getElementById("txt-start").value);
+  const endNum = parseInt(document.getElementById("txt-end").value);
+  const displayArea = document.getElementById("start-end-display");
+  displayArea.innerHTML = "";
+  const errorDisplay = document.getElementById("error-num-order");
+  errorDisplay.innerHTML = ""; //clear out any previous errors
+  const favMessageP = document.getElementById("fav-message");
+  favMessageP.innerHTML = "";
+
+  if (endNum < startNum) {
+    errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`;
+    return; //don't do anything else if you already have an error
   }
+
+  for (let i = startNum; i <= endNum; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = i;
+    displayArea.append(li);
+    //attach event to onclick li
+    li.onclick = () => {
+      favMessageP.innerHTML = `You clicked ${i}.`;
+    };
+  }
+};
+
+let count = 0;
+let updateCount;
+
+//let's loop through the numbers and display a count
+document.querySelector("#btn-count").onclick = (event) => {
+  //start the counter
+  if (event.currentTarget.innerHTML.toLowerCase() == "start") {
+    event.currentTarget.innerHTML = "Stop";
+
+    updateCount = setInterval(() => {
+      count++;
+      document.querySelector("#btn-count").innerHTML = count;
+    }, 300);
+  }
+  //stop the counter
+  else {
+    event.currentTarget.innerHTML = "Start";
+    clearInterval(updateCount);
+  }
+};
+
+document.querySelector("#reset-btn").onclick = () => {
+
+  count = 0;
+};
+
+document.getElementById("show-fruit-btn").onclick = (event) => {
+  event.currentTarget.disabled = true;
+
+
+  //Stupid Dumb way
+  const fruits = {"peach" : "tasty", "strawberry": "yum",
+     "pinapple" : "strong", "Mango": "sweet"};
+  const fruitBasket = document.getElementById("fruit-basket");
+  //Based way
+  fruits.forEach((toy, i)=>{
+    const fruitLi = document.createElement("li")
+    fruitLi.innerHTML = `${toy.key}, ${toy.value}`;
+    fruitBasket.append(fruitLi);
+  })
+
+
 };
