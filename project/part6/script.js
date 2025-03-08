26ca37ea9ebd8b346d94eb3ff902098063aedede
jaervin1 <jaervin@email.sc.edu>
2025-03-08 01:46:31 -0500

    more json

--- project/part6/script.js
+++ project/part6/script.js
@@ -2,6 +2,104 @@ document.getElementById("hamburger-toggle").onclick = () => {
   document.querySelector(".nav-links").classList.toggle("nav-hidden");
 };
 
+class Activity {
+  constructor(
+    name,
+    id,
+    author,
+    location,
+    description,
+    length,
+    type,
+    difficulty,
+    rating,
+    activityType,
+    reviews,
+    pictures
+  ) {
+    this.name = name;
+    this.id = id;
+    this.author = author;
+    this.location = location;
+    this.description = description;
+    this.length = length;
+    this.type = type;
+    this.difficulty = difficulty;
+    this.rating = rating;
+    this.activityType = activityType;
+    this.reviews = reviews;
+    this.pictures = pictures;
+  }
+
+  get getActivityPopup() {
+    const activityPopup = document.createElement("div");
+
+        let activityContent = document.createElement("div");
+        activityContent.classList.add("activity-content");
+
+        let infoSection = document.createElement("section");
+        infoSection.classList.add("activity-info");
+
+        let activityName = document.createElement("h1");
+        activityName.textContent = this.name;
+
+        let activityLocation = document.createElement("h3");
+        activityLocation.textContent = this.location;
+
+        let divider = document.createElement("hr");
+
+        let activityList = document.createElement("ul");
+        activityList.id = "activity-add-info";
+
+        let lengthItem = document.createElement("li");
+        lengthItem.textContent = `Length: ${this.length} miles`;
+
+        let difficultyItem = document.createElement("li");
+        difficultyItem.textContent = `Difficulty: ${this.difficulty}`;
+
+        let typeItem = document.createElement("li");
+        typeItem.textContent = `Type: ${this.type}`;
+
+        let ratingItem = document.createElement("li");
+        let starRating = this.createStarRating();
+        ratingItem.appendChild(starRating);
+        ratingItem.innerHTML += ` (${this.reviews.length} reviews)`;
+
+        activityList.append(lengthItem, difficultyItem, typeItem, ratingItem);
+
+        let activityDescription = document.createElement("p");
+        activityDescription.id = "activity-description";
+        activityDescription.textContent = this.description;
+
+        infoSection.append(activityName, activityLocation, divider, activityList, activityDescription);
+
+        let imageSection = document.createElement("section");
+        imageSection.classList.add("activity-image");
+
+        let activityImage = document.createElement("img");
+        activityImage.src = this.pictures[0] || "https://placehold.co/500x250"; // Use default if no image provide
+
+        imageSection.appendChild(activityImage);
+        activityContent.append(infoSection, imageSection);
+
+        return activityContent;
+    }
+  
+
+  createStarRating() {
+    let ratingContainer = document.createElement("span");
+    for (let i = 0; i < 5; i++) {
+      let star = document.createElement("span");
+      star.classList.add("fa", "fa-star");
+      if (i < Math.round(this.rating)) {
+        star.classList.add("checked");
+      }
+      ratingContainer.appendChild(star);
+    }
+    return ratingContainer;
+  }
+}
+
 const getActivities = async () => {
   const url = "https://jaervin1.github.io/json/activity.json";
   try {
@@ -16,7 +114,21 @@ const showActivities = async () => {
   const activities = await getActivities();
   const activitiesSection = document.getElementById("explore");
 
-  activities.forEach((activity) => {
+  activities.forEach((activityData) => {
+    const activity = new Activity(
+      activityData.name,
+      activityData.id,
+      activityData.author,
+      activityData.location,
+      activityData.description,
+      activityData.length,
+      activityData.type,
+      activityData.difficulty,
+      activityData.rating,
+      activityData.activityType,
+      activityData.reviews,
+      activityData.pictures
+    );
     const activitySection = document.createElement("div");
     const activityInfo = document.createElement("div");
 
@@ -32,9 +144,7 @@ const showActivities = async () => {
       star.classList.add("fa", "fa-star");
       if (i <= rating) {
         star.classList.add("checked");
-        console.log(rating);
-      }
-      else {
+      } else {
         star.classList.add("unchecked");
       }
       activityInfo.appendChild(star);
@@ -50,7 +160,7 @@ const showActivities = async () => {
     image.src =
       activity.pictures && activity.pictures.length > 0
         ? activity.pictures[0] // First Picture
-        : "images/default.jpg"; //Default 
+        : "images/default.jpg"; //Default
 
     activityImage.appendChild(image);
 
@@ -61,6 +171,14 @@ const showActivities = async () => {
     activitySection.appendChild(activityImage);
 
     activitySection.classList.add("result", "content");
+
+    const modalContent = document.getElementById("modalContent");
+    
+    activitySection.onclick = () => {
+      modalContent.innerHTML = "";
+      modalContent.appendChild(activity.getActivityPopup);
+      document.getElementById("activityModal").style.display = "block";
+    };
 
     activitiesSection.appendChild(activitySection);
   });
